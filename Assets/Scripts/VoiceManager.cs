ff3bc48b11b9f96a6d7e7895e0cf5fbc228abf77
anTe <77624934+Ante-237@users.noreply.github.com>
2024-05-08 23:20:12 +0200

    item changes

--- Assets/Scripts/VoiceManager.cs
+++ Assets/Scripts/VoiceManager.cs
@@ -10,6 +10,16 @@ public class VoiceManager : MonoBehaviour
     [SerializeField] private AppVoiceExperience AppVoice;
     [SerializeField] private TextMeshProUGUI TranscriptText;
     [SerializeField] private TextMeshProUGUI DebugText;
+    [SerializeField] private TextMeshProUGUI HintText;
+    [SerializeField, Range(0, 10f)] private float StartTime = 3.0f;
+    [SerializeField] private List<GameObject> ObjectList = new List<GameObject>();
+    [SerializeField] private List<string> hintString = new List<string>();
+    [SerializeField] private Transform SpawnPoint;
+
+
+
+    private string RepeatText = "What do you see?";
+
 
     // text to voice real time translation.
     [SerializeField] private TTSSpeaker TTSSpeaker;
@@ -20,35 +30,101 @@ public class VoiceManager : MonoBehaviour
     // show confidence
     // show what's being said by the player. 
 
+    private int currentIndex = 0;
+    private GameObject currentObject;
+
     private void Start()
     {
         AppVoice.TranscriptionEvents.OnFullTranscription.AddListener(UserVoiceInput);
-        AppVoice.TranscriptionEvents.OnPartialTranscription.AddListener(UserVoiceInputPartial);
-
         //AppVoice.VoiceEvents.
+        StartCoroutine(InitCallBack());
+        SpawnObject();
     }
 
-    public void OutputText(string[] text)
+    private void SpawnObject()
     {
-        foreach(string s in text)
+       currentObject =  Instantiate(ObjectList[currentIndex], SpawnPoint.position, Quaternion.identity);
+    }
+
+    IEnumerator NextLevelSequence()
+    {
+        yield return new WaitForSeconds(StartTime);
+        if(currentIndex < ObjectList.Count)
         {
-            DebugText.text = s;
+            currentIndex++;
+            if(currentObject != null)
+            {
+                Destroy(currentObject);
+            }
+            SpawnObject();
         }
     }
 
-    public void WrongOutput(string text)
+
+
+    public void ResponseCheck(string[] arg)
     {
-        DebugText.text = text;
+       foreach(string s in arg)
+        {
+            if(s == "object_plunger" )
+            {
+                if( currentIndex == 0)
+                {
+                    StartCoroutine(NextLevelSequence());
+                }
+            }
+
+            else if(s == "object_camera")
+            {
+                if (currentIndex == 1)
+                {
+                    StartCoroutine(NextLevelSequence());
+                }
+            }
+
+            else if(s == "object_streetrat")
+            {
+                if (currentIndex == 2)
+                {
+                    StartCoroutine(NextLevelSequence());
+                }
+            }
+
+            else if(s == "woodenaxe")
+            {
+                if (currentIndex == 3)
+                {
+                    StartCoroutine(NextLevelSequence());
+                }
+            }
+            else
+            {
+                // surely wrong output.
+                WrongResponseText();
+                TranscriptText.text = "<color=red>wrong response</color>";
+            }
+        }
+    }
+
+    IEnumerator InitCallBack()
+    {
+        yield return new WaitForSeconds(StartTime);
+        ReapeatText();
+    }
+
+    private void ReapeatText()
+    {
+        TTSSpeaker.SpeakQueued(RepeatText);
+    }
+
+    private void WrongResponseText()
+    {
+        TTSSpeaker.SpeakQueued("Wrong response");
     }
 
     void UserVoiceInput(string arg0)
     {
         TranscriptText.text = arg0;
-    }
-
-    void UserVoiceInputPartial(string arg0)
-    {
-        TranscriptText.text = arg0 ;
     }
 
     private void Update()
@@ -57,7 +133,11 @@ public class VoiceManager : MonoBehaviour
         {
             AppVoice.Activate();
         }
+
+        if (OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            // show hint or update current hint.
+            HintText.text = hintString[currentIndex];
+        }
     }
-
-
 }
